bcd16ccdcdb6f24741fbabf8863cb080aadefed0
Shell Branch <sbranch@homeaway.com>
2019-05-24 08:50:11 -0700

    App now has functioning login and log out links that updates the navigation depending on if a user is signed in or out

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,13 +4,15 @@ import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    //console.log(auth)
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
     return (
         <nav className="nav-wrapper grey darken-3">
         <div className="container">
         <Link to='/' className='brand-logo'>ShellPlan</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        { links }
         </div>
         </nav> 
 
@@ -18,9 +20,8 @@ const Navbar = () => {
 }
 
 const mapStateToProps = (state) => {
-console.log(state)
  return {
-
+    auth: state.firebase.auth
  }
 }
 
