e7b44ed744b2f8751c55a5e9cd90cfcef7229b86
Shelly Branch <sbranch@homeaway.com>
2019-06-05 08:02:36 -0700

    Authetication is complete

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -10,7 +10,9 @@ const SignedInLinks = (props) => {
         <ul className="right">
         <li><NavLink to='/create/'>New Project</NavLink></li>
         <li><a onClick={ props.signOut }>Sign Out</a></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">SN</NavLink></li>
+        <li><NavLink to='/' className="btn btn-floating pink lighten-1">
+        { props.profile.initials}
+        </NavLink></li>
         </ul>
 
     )
